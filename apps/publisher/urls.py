25bd7c1bdc7f926a3066243dcba9b3feaaddcfe7
James-Crean <creanjames@gmail.com>
2016-04-05 15:38:31 -0700

    commit 3

--- apps/publisher/urls.py
+++ apps/publisher/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
 ]
--- apps/publisher/views.py
+++ apps/publisher/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader, Context
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'base.html')
+
+def register(request):
+  return HttpResponse("Register page")
+
+def login(request):
+  return HttpResponse("login page")
