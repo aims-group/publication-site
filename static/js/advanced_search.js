044b442bc790dfbbfeaa8dbe85f05dfad20062e5
Crean <crean2@llnl.gov>
2017-08-16 15:36:16 -0700

    More css. Removed a console log

--- static/js/advanced_search.js
+++ static/js/advanced_search.js
@@ -4,7 +4,6 @@ $('#meta-filter').keyup(function() {
     var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
     meta.show().filter(function() {
         var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-        console.log(text);
         return !~text.indexOf(val);
     }).hide();
 });
