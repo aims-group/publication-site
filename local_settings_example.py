557f4251ee5cab4ac01957c607caf8604f42b0d8
Crean <crean2@llnl.gov>
2017-09-22 10:30:35 -0700

    Adds an example local_settings file

---
+++ local_settings_example.py
@@ -0,0 +1,22 @@
+# An example file demonstrating what the minimum local_settings.py should contain.
+# The values here are not suitable for a production environment. 
+# When deploying this site, a production database such as mySQL or Postrgres should be used instead of sqlite.
+# We recommend running Django's built in deployment check via 'python manage.py check --deploy'
+
+DEBUG = True # Gives lots of details when something goes wrong. Only set to True for development
+
+# sqlite is the quick an easy development db
+DATABASES = {
+  'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': 'local.db',
+      'USER': '',             # Not used with sqlite3.
+      'PASSWORD': '',         # Not used with sqlite3.
+      'HOST': '',             # Not used with sqlite3.
+      'PORT': '',             # Not used with sqlite3.
+  }
+}
+
+SECRET_KEY = "}.!CLP*DF.CY=|DY<>~56?2N0I~UF^O>40'M|Y!)%E>6%<'95N" # Used by danjo for cryptographic signing. Set this to a unique random value
+RECAPTCHA_PUBLIC_KEY = "" # Google's recaptcha. Refer to the recaptcha web site for this key
+RECAPTCHA_PRIVATE_KEY = "" # https://www.google.com/recaptcha/intro/
