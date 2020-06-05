c9a6b7e208c347c650d6593fe421e2fd470768bc
mauzey1 <mauzey1@llnl.gov>
2020-06-05 12:26:14 -0700

    Set STATIC_ROOT for static directory to be populated by `python manage.py collectstatic`

--- apps/admin/settings.py
+++ apps/admin/settings.py
@@ -96,6 +96,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = 'static'
+
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
