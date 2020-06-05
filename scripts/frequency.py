489ba628686723c20fc0ab17d699e98710f107a6
mauzey1 <mauzey1@llnl.gov>
2020-06-04 19:35:34 -0700

    Add experiments, frequencies, and models for CMIP6.

--- scripts/frequency.py
+++ scripts/frequency.py
@@ -13,7 +13,22 @@ frequency_data = [
      ]},
     {'project_name': 'CMIP6',
      'frequencies': [
-
+         '1hr',
+         '1hrCM',
+         '1hrPt',
+         '3hr',
+         '3hrPt',
+         '6hr',
+         '6hrPt',
+         'day',
+         'dec',
+         'fx',
+         'mon',
+         'monC',
+         'monPt',
+         'subhrPt',
+         'yr',
+         'yrPt'
      ]},
     {'project_name': 'AIMS',
      'frequencies': [
