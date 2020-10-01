cc3e50249d1b7d6a2e5495b448710032a2262e2c
mauzey1 <mauzey1@llnl.gov>
2020-09-30 17:17:27 -0700

    Edit journal warning message

--- pubhub-static/js/editform.js
+++ pubhub-static/js/editform.js
@@ -121,8 +121,9 @@ function doisearch() {
                         .html( 'The journal publishing this article, <b>' + data.container_title + '</b>, was not found in the list.'
                              + ' To add this journal, please submit an issue to our <a href="https://github.com/aims-group/publication-site/issues/">GitHub Issues</a> page'
                              + ' with the title: <br> <b>"Please add journal named: ' + data.container_title + '."</b> <br><br>'
-                             + ' Currently, your publication will be entered with journal set to "Other".  You can change the journal of this publication'
-                             + ' after it has been added to the list by going to the Edit page and selecting your publication title.')
+                             + ' Currently, your publication will be registered with journal set to "Other".  Once you\'ve submitted the issue,'
+                             + ' as described above, and the journal name you requested has been added to the list, please go to the Edit'
+                             + ' page, select your publication, and replace "Other" with the actual name of the journal.')
                         .addClass('alert alert-warning');
                         $('#journal-warning').append(warn);
                     }
