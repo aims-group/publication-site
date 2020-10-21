a9a98c04a498a2792999e186038de1818fab2fbb
mauzey1 <mauzey1@llnl.gov>
2020-10-21 13:11:53 -0700

    Remove import of settings in admin/urls.py

--- apps/admin/urls.py
+++ apps/admin/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
 from publisher.forms import RegistrationViewUniqueEmail
 from django.contrib.auth.views import logout_then_login
 
