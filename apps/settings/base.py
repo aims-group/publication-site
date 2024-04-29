19c8861efce6a51cc98207253a8d002ae841b47a
mauzey1 <mauzey1@llnl.gov>
2024-04-29 14:31:50 -0700

    Move most of the settings to the base settings file

--- apps/settings/base.py
+++ apps/settings/base.py
@@ -1,3 +1,8 @@
+import os
+
+DEBUG = os.environ.get("DJANGO_DEBUG", False)
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
 CSRF_COOKIE_HTTPONLY = True
 # Application definition
 
@@ -24,6 +29,34 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'publisher.processors.nav_options',
+                'publisher.processors.pending_dois'
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 ROOT_URLCONF = 'admin.urls'
 
 WSGI_APPLICATION = 'admin.wsgi.application'
@@ -46,6 +79,31 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse"
+        }
+    },
+    "handlers": {
+        "applogfile": {
+            "level": os.environ.get("DJANGO_LOGGING_LEVEL", default="INFO"),
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename":  os.environ.get("DJANGO_LOGGING_FILENAME", default="/var/log/publications-site.log"),
+            "maxBytes": 1024 * 1024 * 15,
+            "backupCount": 10
+        }
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["applogfile"],
+            "level": os.environ.get("DJANGO_LOGGING_LEVEL", default="INFO"),
+            "propagate": True
+        },
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
@@ -76,8 +134,16 @@ LOGIN_REDIRECT_URL = '{}/'.format(URL_PREFIX)
 LOGIN_URL = '{}/accounts/login/'.format(URL_PREFIX)
 
 # Django Admin URL.
-ADMIN_URL = "admin/"
+ADMIN_URL = os.environ.get("DJANGO_ADMIN_URL", default="admin/")
 
 STATICFILES_DIRS = (
     'pubhub-static',
 )
+
+# Google's recaptcha. Refer to the recaptcha web site for this key https://www.google.com/recaptcha/intro/
+RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY", default='')
+RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY", default='')
+
+# Email values must be set for password recovery to function
+EMAIL_HOST = os.environ.get("EMAIL_HOST", default='localhost')
+EMAIL_PORT = os.environ.get("EMAIL_PORT", default=1025)
--- apps/settings/local.py
+++ apps/settings/local.py
@@ -2,88 +2,23 @@ import os
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", False)
+DEBUG = True
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'publisher.processors.nav_options',
-                'publisher.processors.pending_dois'
-            ],
-        },
-    },
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-DATABASES = {
-  'default': {
-      'ENGINE': 'django.db.backends.postgresql',
-      'NAME': os.environ.get('POSTGRES_DB', default=''),
-      'USER': os.environ.get('POSTGRES_USER', default=''),             # Not used with sqlite3.
-      'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default=''),         # Not used with sqlite3.
-      'HOST': os.environ.get('POSTGRES_HOST', default=''),             # Not used with sqlite3.
-      'PORT': os.environ.get('POSTGRES_PORT', default=''),             # Not used with sqlite3.
-  }
-}
-
-LOGGING = {
- "version": 1,
- "disable_existing_loggers": False,
- "filters": {
-   "require_debug_false": {
-     "()": "django.utils.log.RequireDebugFalse"
-   }
- },
- "handlers": {
-   "applogfile": {
-     "level": os.environ.get("DJANGO_LOGGING_LEVEL", default="INFO"),
-     "class": "logging.handlers.RotatingFileHandler",
-     "filename":  os.environ.get("DJANGO_LOGGING_FILENAME", default="/var/log/publications-site.log"),
-     "maxBytes": 1024 * 1024 * 15,
-     "backupCount": 10
-   }
- },
- "loggers": {
-   "django.request": {
-     "handlers": ["applogfile"],
-     "level": os.environ.get("DJANGO_LOGGING_LEVEL", default="INFO"),
-     "propagate": True
-   },
- }
-}
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", default='29l93ms9m32390#20-z!32ad38')
 
-# Google's recaptcha. Refer to the recaptcha web site for this key https://www.google.com/recaptcha/intro/
-RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY", default='')
-RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY", default='')
-
-# Email values must be set for password recovery to function
-EMAIL_HOST = os.environ.get("EMAIL_HOST", default='localhost')
-EMAIL_PORT = os.environ.get("EMAIL_PORT", default=1025)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', default=''),
+        'USER': os.environ.get('POSTGRES_USER', default=''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default=''),
+        'HOST': os.environ.get('POSTGRES_HOST', default=''),
+        'PORT': os.environ.get('POSTGRES_PORT', default=''),
+    }
+}
 
 # os.environ["REQUESTS_CA_BUNDLE"] = "/absolute/path/to/certfile.crt"  
 # Path to the .crt file. 
 # Used if the server is behind a corporate firewall that intercepts ssl
 # This can be set to "" (empty string) if your server is not behind one of these annoying firewalls
-
-# Django Admin URL regex.
-ADMIN_URL = os.environ.get("DJANGO_ADMIN_URL")
