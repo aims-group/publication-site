2a735376a3b43bb9b13d96735a6a6a7b22c8b3e6
mauzey1 <mauzey1@llnl.gov>
2024-05-02 09:31:36 -0700

    Remove MIDDLEWARE_CLASSES section in settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,18 +16,6 @@ INSTALLED_APPS = [
     'publisher',
     'django_recaptcha',
     'widget_tweaks',
-    'gunicorn',
-]
-
-MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 TEMPLATES = [
