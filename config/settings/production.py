d3d310ebec5fc394a36356a6261e2ad317c69e7b
mauzey1 <mauzey1@llnl.gov>
2024-05-02 09:54:07 -0700

    Add production settings file

---
+++ config/settings/production.py
@@ -0,0 +1,70 @@
+import os
+from .base import *
+
+
+# GENERAL
+# ------------------------------------------------------------------------------
+DEBUG = False
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
+
+# DATABASES
+# ------------------------------------------------------------------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', default=''),
+        'USER': os.environ.get('POSTGRES_USER', default=''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default=''),
+        'HOST': os.environ.get('POSTGRES_HOST', default=''),
+        'PORT': os.environ.get('POSTGRES_PORT', default=''),
+    }
+}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES["default"]["CONN_MAX_AGE"] = os.environ.get( 
+    "CONN_MAX_AGE", default=60
+)
+
+
+# SECURITY
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+SECURE_SSL_REDIRECT = os.environ.get("DJANGO_SECURE_SSL_REDIRECT", default=True)
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+SESSION_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+SECURE_HSTS_SECONDS = 60
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+SECURE_HSTS_INCLUDE_SUBDOMAINS = os.environ.get(
+    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
+SECURE_HSTS_PRELOAD = os.environ.get("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+# https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
+SECURE_CONTENT_TYPE_NOSNIFF = os.environ.get(
+    "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
+)
+
+
+# APPS
+# ------------------------------------------------------------------------------
+INSTALLED_APPS += ['gunicorn']
+
+
+# STATIC
+# ------------------------
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+# os.environ["REQUESTS_CA_BUNDLE"] = "/absolute/path/to/certfile.crt"  
+# Path to the .crt file. 
+# Used if the server is behind a corporate firewall that intercepts ssl
+# This can be set to "" (empty string) if your server is not behind one of these annoying firewalls
