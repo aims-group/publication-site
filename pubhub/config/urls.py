d55d0a77608994853db01a859cf0a722c0554cb5
Chris Mauzey <mauzey1@llnl.gov>
2026-08-17 17:32:09 -0700

    Fix issue with logout link

--- pubhub/config/urls.py
+++ pubhub/config/urls.py
@@ -2,11 +2,11 @@ from django.urls import include, path
 from django.contrib import admin
 from django.conf import settings
 from publisher.forms import RegistrationViewUniqueEmail
-from django.contrib.auth.views import logout_then_login
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-    path('accounts/logout/', logout_then_login),
+    path('accounts/logout/', LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
     path('accounts/register/', RegistrationViewUniqueEmail.as_view(template_name="django_registration/registration_form.html"), name='register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
